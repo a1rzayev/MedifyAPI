e70c03b7e4ac64a2ddf9eccdd04483abb44138bd
a1rzayev <asifrzayev2006@example.com>
2025-01-03 15:36:16 +0400

    Connect JWTBareer

--- src/MedifyAPI.Api/Program.cs
+++ src/MedifyAPI.Api/Program.cs
@@ -35,6 +35,28 @@ builder.Services.AddScoped<ILogService, LogService>();
 builder.Services.AddDbContext<MedifyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MsSql"))
 );
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.`
@@ -43,6 +65,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
